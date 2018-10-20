76c6bd3fd4f03600f35f85d7b614a21e75fb2259
carlos rivero <carlitos.vzla@gmail.com>
2018-10-20 19:02:33 -0300

    Agregado .gitignore para carpeta vendor y composer.lock codificación de los caracteres en los seeders mostraba mal las comillas simples.

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -13,29 +13,29 @@ class RoleTableSeeder extends Seeder
     {
         //
         $role_driver = new Role();
-        $role_driver->name = ‘driver’;
-        $role_driver->description = ‘A Driver User’;
+        $role_driver->name = 'driver';
+        $role_driver->description = 'A Driver User';
         $role_driver->save();
 
         $role_manager = new Role();
-        $role_manager->name = ‘manager’;
-        $role_manager->description = ‘A Manager User’;
+        $role_manager->name = 'manager';
+        $role_manager->description = 'A Manager User';
         $role_manager->save();
 
 
         $role_admin = new Role();
-        $role_admin->name = ‘admin’;
-        $role_admin->description = ‘System Administrator’;
+        $role_admin->name = 'admin';
+        $role_admin->description = 'System Administrator';
         $role_admin->save();
 
         $role_specialist = new Role();
-        $role_specialist->name = ‘especialist’;
-        $role_specialist->description = ‘Especialist’;
+        $role_specialist->name = 'especialist';
+        $role_specialist->description = 'Especialist';
         $role_specialist->save();
 
         $role_client = new Role();
-        $role_client->name = ‘client’;
-        $role_client->description = ‘Client’;
+        $role_client->name = 'client';
+        $role_client->description = 'Client';
         $role_client->save();
     }
 
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,34 +11,34 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $role_driver = Role::where(‘name’, ‘driver’)->first();
-        $role_manager  = Role::where(‘name’, ‘manager’)->first();
+        $role_driver = Role::where('name', 'driver')->first();
+        $role_manager  = Role::where('name', 'manager')->first();
 
         $driver = new User();
-        $driver->name = ‘Driver Name’;
-        $driver->email = ‘driver@ag.com’;
-        $driver->password = bcrypt(‘123456’);
+        $driver->name = 'Driver Name';
+        $driver->email = 'driver@ag.com';
+        $driver->password = bcrypt('123456');
         $driver->save();
         $driver->roles()->attach($role_driver);
 
         $manager = new User();
-        $manager->name = ‘Manager Name’;
-        $manager->email = ‘manager@ag.com’;
-        $manager->password = bcrypt(‘123456’);
+        $manager->name = 'Manager Name';
+        $manager->email = 'manager@ag.com';
+        $manager->password = bcrypt('123456');
         $manager->save();
         $manager->roles()->attach($role_manager);
 
         $client = new User();
-        $client->name = ‘Cliente Perez’;
-        $client->email = ‘client@ag.com’;
-        $client->password = bcrypt(‘123456’);
+        $client->name = 'Cliente Perez';
+        $client->email = 'client@ag.com';
+        $client->password = bcrypt('123456');
         $client->save();
         $client->roles()->attach($role_client);
 
         $specialist = new User();
-        $specialist->name = ‘Maria Especialista’;
-        $specialist->email = ‘specialist@ag.com’;
-        $specialist->password = bcrypt(‘123456’);
+        $specialist->name = 'Maria Especialista';
+        $specialist->email = 'specialist@ag.com';
+        $specialist->password = bcrypt('123456');
         $specialist->save();
         $specialist->roles()->attach($role_client);
 
