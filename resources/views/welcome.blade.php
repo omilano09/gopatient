5a9f24ce292fd600cf12acf220ab6d946b61c95b
Gloris <glorisvargas@gmail.com>
2018-10-20 23:55:31 -0400

    Funcionalidad logo @gvargas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,17 +78,13 @@
             @endif
 
             <div class="content">
+                <div><img src="/img/logo.jpg"></div>
                 <div class="title m-b-md">
-                    GoPacient
+                    GoPatient
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
                 </div>
             </div>
         </div>
