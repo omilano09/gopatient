da6116466dfa948035077af94a5a5442338539a9
carlos rivero <carlitos.vzla@gmail.com>
2018-10-21 04:05:46 -0300

    Comento los sedders de usuario y roles hasta que se suba el modelo Rol al repo.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // Role comes before User seeder here.
-        $this->call(RoleTableSeeder::class);
+        //$this->call(RoleTableSeeder::class);
         // User seeder will use the roles above created.
-        $this->call(UserTableSeeder::class);
+        //$this->call(UserTableSeeder::class);
         $this->call(VehicleTypesTableSeeder::class);
         $this->call(TripStatusTableSeeder::class);
         $this->call(AddresTypeTableSeeder::class);
