1811e09ec1e3fd965f74d29f5e98185e0b281e8f
omilano09 <35047359+omilano09@users.noreply.github.com>
2018-10-22 22:21:01 -0400

    Oralis Milano Cambio en objeto de BD users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('last-name');
+            $table->string('username');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->string('type_user');
+            $table->string('status');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
